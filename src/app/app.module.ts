11dc30a91ab7864f09517135780eee8cda1f5380
ap94sd24 <adampan94@gmail.com>
2018-10-26 09:23:10 -0700

    Task Manager App Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    PostCreateComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
