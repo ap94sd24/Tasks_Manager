93f959f71d7a163521ffe716b14d0bb1edb9987b
ap94sd24 <adampan94@gmail.com>
2018-12-07 15:50:34 -0800

    TESTING FIX

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+     );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+  next();
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/todo-app'));
 
