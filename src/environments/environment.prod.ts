9d77d832947bc49ba22ae79f6687409336edf5fe
ap94sd24 <adampan94@gmail.com>
2018-12-06 16:44:30 -0800

    Fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  routes_url: 'http://forumapp2-env.nmjhumbfnc.us-west-2.elasticbeanstalk.com/api'
+  routes_url: 'https://protected-coast-20726.herokuapp.com/api'
 };
+
