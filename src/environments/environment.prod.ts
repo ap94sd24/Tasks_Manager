acb491fcf6346940599b4c9e3363ee0898fa82e3
ap94sd24 <adampan94@gmail.com>
2018-12-07 14:49:26 -0800

    Fixing 2 app deployment - heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  routes_url: 'https://protected-coast-20726.herokuapp.com/api'
+  routes_url: 'https://stark-wildwood-62532.herokuapp.com/api'
 };
 
