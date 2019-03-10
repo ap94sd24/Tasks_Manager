a0db4374419070b190e569301857aa5db84739ee
ap94sd24 <adampan94@gmail.com>
2019-03-10 15:46:31 -0700

    Updated schemas

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
+  date: { type: Date, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
   imagePath: {type: String, required: false},
+  community: {type: String, required: false},
+  votes: {type: Number, required: false},
+  links: {type: mongoose.Schema.Types.Array, required: false},
   creator: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}
 });
 
