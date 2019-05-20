29eb818563861658968e0f5cc8f18cb7b3365831
ap94sd24 <adampan94@gmail.com>
2019-05-20 15:05:27 -0700

    Deploying front-end to heroku w/ aws ebs backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  routes_url: 'https://stark-wildwood-62532.herokuapp.com/api'
+  //routes_url: 'https://stark-wildwood-62532.herokuapp.com/api'
+  routes_url: 'http://caffeenreader.us-east-2.elasticbeanstalk.com/api'
 };
 
